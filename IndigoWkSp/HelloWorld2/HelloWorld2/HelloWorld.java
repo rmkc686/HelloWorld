b12bbdb47a06241b725fe06295def8ccb9bf0664
rmkc <rmkc23@gmail.com>
2015-01-20 13:21:42 -0800

    Initial checkin of HelloWorld

---
+++ IndigoWkSp/HelloWorld2/HelloWorld2/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld2;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
